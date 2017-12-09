09f91497fcbbcbea2ab7663465a32e84a365d0b7
Kushal Hada <kushaldeveloper@gmail.com>
2017-12-09 18:50:09 -0500

    try testing

---
+++ backend/my_message/MyMessage.py
@@ -0,0 +1,47 @@
+from shared import db
+
+
+class MyMessage(db.Model):
+    id = db.Column(db.String(40), primary_key=True)
+    subject = db.Column(db.UnicodeText, nullable=False)
+    sender = db.Column(db.String(255), nullable=False)
+    sent = db.Column(db.DateTime, nullable=False)
+    receiver = db.Column(db.String(255), nullable=False)
+    received = db.Column(db.DateTime, nullable=False)
+    body = db.Column(db.UnicodeText, nullable=False)
+    score = db.Column(db.Float, nullable=False, default=0.0)
+
+    def __init__(
+            self,
+            message_id,
+            subject,
+            sender,
+            sent,
+            receiver,
+            received,
+            body):
+        self.id = message_id
+        self.subject = subject
+        self.sender = sender
+        self.sent = sent
+        self.receiver = receiver
+        self.received = received
+        self.body = body
+
+    def __repr__(self):
+        return '<MyMessage %r>' % self.id
+
+    @staticmethod
+    def get_latest_message():
+        return MyMessage.query
+
+    @property
+    def serialize(self):
+        return {
+            "id": self.id,
+            "subject": self.subject,
+            "sender": self.sender,
+            "sent": self.sent,
+            "receiver": self.receiver,
+            "body": self.body
+        }
---
+++ backend/routes/MyMessageRoute.py
@@ -0,0 +1,56 @@
+import string
+import uuid
+from datetime import datetime
+
+import random
+from flask import Blueprint
+
+import shared
+from my_message.MyMessage import MyMessage
+
+MyMessageRoute = Blueprint('my_message_route', __name__)
+
+
+@MyMessageRoute.route('/api/v0/my_message/last')
+def get_latest_message():
+    return MyMessage.get_latest_message()
+
+
+@MyMessageRoute.route('/api/v0/my_message/add')
+def hello_world():
+    create_message()
+    return 'Hello World!'
+
+
+def get_random_string(length):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(length))
+
+
+def create_message():
+    my_message = MyMessage(
+        message_id=uuid.uuid4(),
+        sender=f"{get_random_string(8)}@{get_random_string(5)}.{get_random_string(3)}",
+        sent=datetime.utcnow(),
+        receiver=f"{get_random_string(8)}@{get_random_string(5)}.{get_random_string(3)}",
+        received=datetime.utcnow(),
+        subject=f"{get_random_string(5)} {get_random_string(8)} {get_random_string(3)}",
+        body=f"{get_random_string(5)} {get_random_string(8)} {get_random_string(6)} {get_random_string(3)}. "
+             f"{get_random_string(5)} {get_random_string(8)} {get_random_string(6)} {get_random_string(3)}. "
+             f"{get_random_string(5)} {get_random_string(8)} {get_random_string(6)} {get_random_string(3)}. "
+             f"{get_random_string(5)} {get_random_string(8)} {get_random_string(6)} {get_random_string(3)}. "
+             f"{get_random_string(5)} {get_random_string(8)} {get_random_string(6)} {get_random_string(3)}. "
+             f"{get_random_string(5)} {get_random_string(8)} {get_random_string(6)} {get_random_string(3)}. "
+             f"{get_random_string(5)} {get_random_string(8)} {get_random_string(6)} {get_random_string(3)}. "
+             f"{get_random_string(5)} {get_random_string(8)} {get_random_string(6)} {get_random_string(3)}. "
+             f"{get_random_string(5)} {get_random_string(8)} {get_random_string(6)} {get_random_string(3)}. "
+             f"{get_random_string(5)} {get_random_string(8)} {get_random_string(6)} {get_random_string(3)}. "
+             f"{get_random_string(5)} {get_random_string(8)} {get_random_string(6)} {get_random_string(3)}. "
+             f"{get_random_string(5)} {get_random_string(8)} {get_random_string(6)} {get_random_string(3)}. "
+             f"{get_random_string(5)} {get_random_string(8)} {get_random_string(6)} {get_random_string(3)}. "
+             f"{get_random_string(5)} {get_random_string(8)} {get_random_string(6)} {get_random_string(3)}. "
+             f"{get_random_string(5)} {get_random_string(8)} {get_random_string(6)} {get_random_string(3)}. "
+             f"{get_random_string(5)} {get_random_string(8)} {get_random_string(6)} {get_random_string(3)}. "
+             f"{get_random_string(5)} {get_random_string(8)} {get_random_string(6)} {get_random_string(3)}. ")
+    shared.db.session.add(my_message)
+    shared.db.session.commit()
