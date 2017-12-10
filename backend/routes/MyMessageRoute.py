4ea93e8ea8bc38c627dd6e454128c56a289a61bd
Kushal Hada <kushaldeveloper@gmail.com>
2017-12-10 12:13:28 -0500

    remove jsonify

--- backend/routes/MyMessageRoute.py
+++ backend/routes/MyMessageRoute.py
@@ -3,7 +3,7 @@ import uuid
 from datetime import datetime
 
 import random
-from flask import Blueprint, jsonify
+from flask import Blueprint
 
 import shared
 from my_message.MyMessage import MyMessage
@@ -13,7 +13,7 @@ MyMessageRoute = Blueprint('my_message_route', __name__)
 
 @MyMessageRoute.route('/api/v0/my_message/last')
 def get_latest_message():
-    return jsonify(MyMessage.query)
+    return MyMessage.query
 
 
 @MyMessageRoute.route('/api/v0/my_message/add')
