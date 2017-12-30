a756860e31bd97680285624c1882fd963d990f00
Kushal Hada <kushaldeveloper@gmail.com>
2017-12-30 13:00:44 -0500

    log visitors to the messages api

--- backend/routes/MyMessageRoute.py
+++ backend/routes/MyMessageRoute.py
@@ -7,6 +7,9 @@ MyMessageRoute = Blueprint('my_message_route', __name__)
 
 @MyMessageRoute.route('/api/v0/my_message/first')
 def get_first_message():
+    from my_message.MyVisitor import save_visitor
+    from flask import request
+    save_visitor(this_request=request)
     result = MyMessage.query.first()
     if result is None:
         return jsonify({"error": "There is no message"})
@@ -15,6 +18,9 @@ def get_first_message():
 
 @MyMessageRoute.route('/api/v0/my_message/add')
 def hello_world():
+    from my_message.MyVisitor import save_visitor
+    from flask import request
+    save_visitor(this_request=request)
     from my_message.MyMessage import create_random_message
     create_random_message().save()
     return render_template("pages/add.html")
@@ -22,6 +28,9 @@ def hello_world():
 
 @MyMessageRoute.route('/api/v0/my_message')
 def get_messages():
+    from my_message.MyVisitor import save_visitor
+    from flask import request
+    save_visitor(this_request=request)
     results = MyMessage.query.all()
     if results is None:
         return jsonify({"error": "There is no message"})
