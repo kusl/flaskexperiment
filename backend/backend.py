f6c3aac8ec17e0d808189ba295d36adeeaafef96
Kushal Hada <kushaldeveloper@gmail.com>
2018-01-01 11:18:44 -0500

    add logging to get visitors as well
    now you can't look without being logged

--- backend/backend.py
+++ backend/backend.py
@@ -39,6 +39,9 @@ def clear():
 
 @app.route('/get')
 def get_visitors():
+    from my_message.MyVisitor import save_visitor
+    from flask import request
+    save_visitor(this_request=request)
     results = MyVisitor.query.all()
     if results is None:
         return jsonify({"error": "There is no visitor"})
