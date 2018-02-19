bd74226b48e0af26afe65d3043e5bd4997a8caf7
Kushal Hada <kushaldeveloper@gmail.com>
2018-02-19 11:35:55 -0500

    fix test
    I don't know how this works

--- backend/test/testMyVisitor.py
+++ backend/test/testMyVisitor.py
@@ -21,6 +21,7 @@ class MyVisitorTestCase(unittest.TestCase):
         self.assertEqual(my_visitor.id, "b297f27e-9544-41d3-b3bc-272e0036d910")
         self.assertEqual(my_visitor.ip, "66.249.83.219")
         self.assertLessEqual(my_visitor.visit_time, datetime.utcnow())
+        print(my_visitor.url)
         self.assertEqual(my_visitor.url, "/test-address")
         self.assertEqual(my_visitor.user_agent, "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0")
 
