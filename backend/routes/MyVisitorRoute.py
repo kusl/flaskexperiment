8d79d0517f624d1895edc014e5571867e91c421b
Kushal Hada <kushaldeveloper@gmail.com>
2018-03-03 00:26:01 -0500

    add a new view with visitors in a table

--- backend/routes/MyVisitorRoute.py
+++ backend/routes/MyVisitorRoute.py
@@ -15,3 +15,17 @@ def get_visitors():
         return jsonify({"error": "There is no visitor"})
     all_visitors = [visitor.serialize for visitor in results]
     return jsonify(all_visitors)
+
+
+@MyVisitorRoute.route('/visitors')
+def display_visitors():
+    from my_message.MyVisitor import save_visitor
+    from flask import request
+    save_visitor(this_request=request)
+    results = MyVisitor.query.all()
+    print(results)
+    if results is None:
+        return jsonify({"error": "There is no visitor"})
+    all_visitors = [visitor.serialize for visitor in results]
+    from flask import render_template
+    return render_template("pages/visitors.html", visitors=results)
